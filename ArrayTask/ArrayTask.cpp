4c2be31a77b417920548492bf6e5b56251d84a32
SuperDru <30354748+SuperDru@users.noreply.github.com>
2017-12-18 12:38:02 +0500

    CommonArray -> TemplateArray

--- ArrayTask/ArrayTask.cpp
+++ ArrayTask/ArrayTask.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include "Header.h"
-#include "Array.h"
+#include <string>
 
 using namespace std;
 
 int main() {
 	try {
-		//Array * P = new Array;
-		int arr[6] = { 1, 3, 4, 5 ,-2, 6 };
-		int arr2[3] = { 1, 3, 4 };
-		Array mas1(6, arr);
-		Array mas2(3, arr2);
+		string arr[6] = { "fg", "fg3", "he", "Best" ,"", "gdsg" };
+		string arr2[3] = { "Ha", " I, ", "Am" };
+		string arr3[3] = { " ", " I, ", "Am" };
+		Array<string> mas1(3, arr3);
+		Array<string> mas2(3, arr2);
 		cout << (mas1 < mas2) << endl;
 		cout << (mas1 > mas2) << endl;
 		cout << (mas1 == mas2) << endl;
 		mas1 = mas1 + mas2 + mas1;
 		cout << mas1;
-		cout << (mas1 - 5 + 5);
+		cout << mas2;
 	}
 	catch (char* e) {
 		cout << e;
--- ArrayTask/Header.h
+++ ArrayTask/Header.h
@@ -3,3 +3,126 @@
 #include <conio.h>
 
 using namespace std;
+
+template <class T>
+class Array {
+private:
+	int size;
+	T *mas;
+public:
+	Array(const Array &mas) {
+		this->size = mas.size;
+		this->mas = new T[size];
+		if (this->mas == NULL)
+			throw "OverflowMemoryException";
+		for (int i = 0; i < this->size; i++) {
+			this[i] = mas[i];
+		}
+	}
+
+	Array(int size, T *mas) {
+		if (size < 0)
+			throw "ArraySizeException";
+		this->mas = new T[size];
+		if (this->mas == NULL)
+			throw "OverflowMemoryException";
+		this->size = size;
+		for (int i = 0; i < this->size; i++) {
+			(this->mas)[i] = mas[i];
+		}
+	}
+
+	Array(int size) {
+		void* temp;
+		if (size < 0)
+			throw "ArraySizeException";
+		this->mas = new T[size];
+		if (this->mas == NULL)
+			throw "OverflowMemoryException";
+		this->size = size;
+	}
+
+	~Array()
+	{
+		delete[]mas;
+		cout << "Array was deleted." << endl;
+	}
+
+	void print() {
+		cout << "[";
+		for (int i = 0; i < this->size - 1; i++) {
+			cout << mas[i] << ", ";
+		}
+		cout << mas[this->size - 1] << "]";
+		cout << endl;
+	}
+
+	friend ostream& operator<<(ostream &stream, const Array<T> &arr) {
+		stream << "[";
+		for (int i = 0; i < arr.size - 1; i++) {
+			stream << arr[i] << ", ";
+		}
+		stream << arr[arr.size - 1] << "]";
+		stream << endl;
+		return stream;
+	}
+
+	T& operator[](int i) const {
+		if (i > size || i < 0)
+			throw "ArrayIndexOutOfBoundsException";
+		return mas[i];
+	}
+
+	Array<T>& operator=(const Array &arr) {
+		this->size = arr.size;
+		delete[]this->mas;
+		this->mas = new T[size];
+		if (this->mas == NULL)
+			throw "OverflowMemoryException";
+		for (int i = 0; i < this->size; i++) {
+			mas[i] = arr[i];
+		}
+		return *this;
+	}
+
+	bool operator<(const Array &arr) {
+		return this->size < arr.size;
+	}
+
+	bool operator>(const Array &arr) {
+		return this->size > arr.size;
+	}
+
+	bool operator==(const Array &arr) {
+		if (arr.size != this->size)
+			return false;
+		for (int i = 0; i < this->size; i++) {
+			if (arr[i] != (*this)[i])
+				return false;
+		}
+		return true;
+	}
+
+	Array<T>& operator+(int c) const {
+		Array *temp = new Array(this->size);
+		for (int i = 0; i < this->size; i++) {
+			(*temp)[i] = (*this)[i] + c;
+		}
+		return *temp;
+	}
+
+	Array<T>& operator-(int c) const {
+		return (*this) + -c;
+	}
+
+	Array<T>& operator+(const Array<T> &arr) const {
+		Array *temp = new Array(this->size + arr.size);
+		for (int i = 0; i < this->size; i++) {
+			(*temp)[i] = (*this)[i];
+		}
+		for (int i = this->size; i < (this->size + arr.size); i++) {
+			(*temp)[i] = arr[i - this->size];
+		}
+		return *temp;
+	}
+};
